dba3365feb011f4d409e3c1694303791eb7c085d
dianamacias1218 <95941568+dianamacias1218@users.noreply.github.com>
2023-03-12 12:31:59 -0500

    Add files via upload

---
+++ Develop/models/index.js
@@ -0,0 +1,39 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: "cascade",
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  //foreignKey: 'product_id'
+  foreignKey: 'category_id',
+  
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'Tag',
+  foreignKey: 'product_id',
+  onDelete: "cascade",
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'Product',
+
+  foreignKey: 'tag_id',
+  onDelete: "cascade",
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
